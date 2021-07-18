0558983223e891fca1dcb2c53d5f9d578a6d3193
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2021-07-18 09:21:04 +0530

    Project inilized , starting designing

---
+++ elit-dynamon-client/pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+    
+      </Head>
+      <div>
+        {/* <main className={styles.homePageBG}>
+          <section>
+            <div className={styles.sideNavbar}></div>
+            <div className={styles.homePageContent}>
+              
+            </div>
+          </section>
+          <footer></footer>
+        </main> */}
+      </div>
+    </>
+  );
+}
