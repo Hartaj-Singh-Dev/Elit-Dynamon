40a34be36c4ff2d66aeddc28d0afc3b33a6d7e73
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2021-07-23 14:40:48 +0530

    Navbar Design almost completed

--- elit-dynamon-client/pages/index.tsx
+++ elit-dynamon-client/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import Navbar from "../components/Navbar/Navbar";
 
 
 export default function Home() {
@@ -10,41 +11,8 @@ export default function Home() {
       <Head>
      <title>Elit Dynamon | Articles for geeks</title>
       </Head>
+      <Navbar/>
           <main className={styles.homePage}>
-            <nav className={styles.sideNavbar}>
-              <div className={styles.mainContentLinks}>
-                <div className="logo">
-                  <Link href="/">
-                    <Image  height={40}  width={90}  src="/Elit Dynamon-SVG.svg" alt="" />
-                  </Link>
-                </div>
-                <div className={styles.allLinks}>
-                  <div className="homeLink">
-                    <Link href="/home">
-                     <h2>Home</h2>
-                    </Link>
-                  </div>
-                  <div className="ArticlesLink">
-                <Link href="/blog">
-                  <h2>Articles</h2>
-                    </Link>
-                  </div>
-                  <div className="AboutUsLink">
-                    <Link href="/aboutus">
-                      <h2>About us</h2>
-                    </Link>
-                  </div>
-                  <div className="contactLink">
-                    <Link href="/contactUs">
-                      <h2>Contact US</h2>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.socialMediaLinks}>
-
-              </div>
-            </nav>
             <section className={styles.homePageContent}>
               <div className={styles.home}>
                 hello
