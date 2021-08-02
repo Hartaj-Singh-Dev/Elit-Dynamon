4cfbf8f12d9aeebbfc0d4a327ea0b062b8577bd8
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2021-08-02 13:33:51 +0530

    almost done with footer

--- elit-dynamon-client/components/Footer/Footer.tsx
+++ elit-dynamon-client/components/Footer/Footer.tsx
@@ -17,7 +17,7 @@ const footer = () => {
                 />
               </div>
               <div className={footStyle.subscribeLetter}>
-		      <h1>Subscribe to Newsletter</h1>
+                <h1>Subscribe to Newsletter</h1>
                 <form method="POST">
                   <input type="email" placeholder="Email . ." />
                   <button type="submit">Subscribe</button>
@@ -25,11 +25,30 @@ const footer = () => {
               </div>
             </div>
           </div>
-          <div className={footStyle.companyServices}></div>
+          <div className={footStyle.companyServices}>
+            <div className={footStyle.servicesLists}>
+              <ul>
+                <h2>Online Presence</h2>
+                <li><a>Discord</a></li>
+                <li><a>Github</a></li>
+                <li><a>Instagram</a></li>
+                <li><a>twitter</a></li>
+              </ul>
+              <ul>
+                <h2>Online Presence</h2>
+                <li><a>Discord</a></li>
+                <li><a>Github</a></li>
+                <li><a>Instagram</a></li>
+                <li><a>twitter</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div className={footStyle.lowerFooter}>
-		<h2>Designed with ❤️ by Team<span> Elit Dynamon</span></h2>
-	</div>
+          <h2>
+            Designed with ❤️ by Team<span> Elit Dynamon</span>
+          </h2>
+        </div>
       </div>
     </>
   );
